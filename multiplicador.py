d1749f3975b303323b2f38f7f32350b300a53b17
MISAGITHAY <125407240+Rabelaqbv@users.noreply.github.com>
2023-04-25 16:01:03 -0300

    Add files via upload

---
+++ multiplicador.py
@@ -0,0 +1,7 @@
+print("Bem vindo ao multiplicador")
+num1=(input("digite um número inteiro:"))
+num2=(input("digite outro número inteiro:"))
+x1=int(num1)
+x2=int(num2)
+result=x1*x2
+print("O resultado da multiplicação é:",result)
---
+++ somador.py
@@ -0,0 +1,6 @@
+num1=input("digite um número:")
+num2=input("digite outro número:")
+x=int(num1)
+y=int(num2)
+resultado=x+y
+print(resultado)
